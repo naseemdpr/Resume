c5e073120c06e5105c46f769aaf4865f9c29c548
M Naseem <naseem12brh@gmail.com>
2020-08-24 14:56:52 +0530

    Updates

---
+++ precache-manifest.eabb05a090705110f7b42e2828aab390.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486dc625c39f9a87a06f1f9f47b5e83c",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "62622bd96b66fae95e26",
+    "url": "/Resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b33c2c2c03aa00f757e",
+    "url": "/Resume/static/js/2.2a7fca84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Resume/static/js/2.2a7fca84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62622bd96b66fae95e26",
+    "url": "/Resume/static/js/main.fe530ffa.chunk.js"
+  },
+  {
+    "revision": "d8bf9c01a61e3dc2cf19",
+    "url": "/Resume/static/js/runtime-main.d69e3caf.js"
+  },
+  {
+    "revision": "917d0da8240da98aecd45d5d17ab087f",
+    "url": "/Resume/static/media/mypic.917d0da8.jpg"
+  }
+]);
